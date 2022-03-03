4b2cae583643e457479ba09b98ce83ae59313183
Andrii <example@gmail.com>
2022-03-03 19:49:13 +0200

    first app

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-"use strict";
+'use strict';
+const numberOfFilm = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+    b = prompt('На сколько оцените его?', ""),
+    c = prompt('Один из последних просмотренных фильмов?', ""),
+    d = prompt('На сколько оцените его?', "");
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
