1c71dec17ed6c3f77e51102411263f06010d7241
Andrii <example@gmail.com>
2022-03-03 17:17:32 +0200

    add h3

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 "use strict";
 
-// const result = confirm("Are you here?");
-// console.log(result);
- 
-// const answer = prompt("Вам есть 18?", "18");
-// console.log(answer);
+let incr = 10,
+    decr = 10;
 
-const answers = [];
-
-answers[0] = prompt("whats your name?", "");
-answers[1] = prompt("whats your familyname?", "");
-answers[2] = prompt("whats your age?", "");
-
-document.write(answers);
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
