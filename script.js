1aa8d1a383228c75ffe2c8dd276a466b9d7601fa
Andrii <example@gmail.com>
2022-03-04 16:18:03 +0200

    task3

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// let num = 20;
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 21;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello");
+// console.log(num);
+
+// // function calc(a, b) {
+// //     return (a + b);
+
+// // }
+// // console.log(calc(4, 3));
+// // console.log(calc(5, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+// const anothernum = ret();
+// console.log(anothernum);
+
+// const loger = function () {
+//     console.log('hello');
+// };
+// loger();
+
+// const calc = (a, b) => a + b;
+
+const str = 'test';
+const arr = [1, 2, 3];
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toUpperCase());
+
+console.log(str.toUpperCase());
+
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.5;
+console.log(Math.round(num));
+
+const test = '12.5px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
