3f2c172a17c35956c267f67cfdde60cc16dd0cac
Andrii <example@gmail.com>
2022-03-09 14:32:03 +0200

    Практика.Модификации модального окна(таймер + showModalByScroll)

--- js/script.js
+++ js/script.js
@@ -100,13 +100,15 @@ window.addEventListener('DOMContentLoaded', () => {
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
     
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+    
     modalTriger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            // modal.classList.toggle('show');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -129,5 +131,16 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
        }
     });  
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight-1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+       } 
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 
 });
