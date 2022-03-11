30c513b50455c7af1ea7b6e790872321496e089c
Andrii <example@gmail.com>
2022-03-11 14:52:53 +0200

    Практика.Установлено npm пакети, запуск JSON сервера локалтно

--- js/script.js
+++ js/script.js
@@ -297,5 +297,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
     
 });
